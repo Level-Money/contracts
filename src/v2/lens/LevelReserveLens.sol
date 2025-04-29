46fa402c0c3772cbe64e9851bce0344466b81473
David Lee <juhyunglee1435@gmail.com>
2025-04-28 20:22:22 -0400

    #882: Validate inputs for strategies (3fa148b)

--- src/v2/lens/LevelReserveLens.sol
+++ src/v2/lens/LevelReserveLens.sol
@@ -37,7 +37,7 @@ contract LevelReserveLens is Initializable, OwnableUpgradeable, UUPSUpgradeable,
     // TODO: update when rewards manager is deployed
     // Immutable values are incompatible with upgradeable contracts (see https://forum.openzeppelin.com/t/upgradable-contracts-instantiating-an-immutable-value/28763/2)
     // Since this contract would be updating an existing proxy's implementation, we're choosing to set this as a constant.
-    address public constant rewardsManager = 0x99B1803A19f73fb726D5EED2307bFD0E193Fd152;
+    address public constant rewardsManager = 0x2EC7bd9E5E7Cc1eeE3FfF689f9f6Bb5179914969;
 
     /**
      * @notice Helper function to get the reserves of the given collateral token.
